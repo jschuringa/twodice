f10f3cb2e5ba53c4a42618349d8df3810015fce4
Joey Schuringa <jschuringa86@gmail.com>
2015-05-27 15:04:02 -0500

    made email into module instead of class
    looks like there are possibly some files that didn't get uploaded last
    time. either that or pychache needs to be in the.gitignore

--- email/email.py
+++ email/email.py
@@ -1,23 +1,13 @@
 import smtplib
 
-class email:
-
+def sendEmail(address, emailText):
     ourAddress = 'twodice395@gmail.com'
     password = 'capstone395'
     server = smtplib.SMTP('smtp.gmail.com:587')
-
-    def __init__(self, address, emailText):
-        self.address = address
-        self.emailText = emailText
-        sendEmail()
-
-    def sendEmail():
-        self.address = 'jschuringa86@gmail.com'
-        self.emailText = 'test'
-        server.starttls()
-        server.login(ourAddress, password)
-        server.sendmail(ourAdress, self.address, self.emailText)
-        server.quit()
+    server.starttls()
+    server.login(ourAddress, password)
+    server.sendmail(ourAdress, address, emailText)
+    server.quit()
 
 
 
--- search/search.py
+++ search/search.py
@@ -1,13 +1,20 @@
 class search:
+
+    matchList = []
+
     def __init__(self, userList, compVals, compCutoff):
         self.userList = userList
         self.compVals = compVals
+        self.compCutoff = compCutoff
         returnMatch()
     
     def returnMatch():
-
+        for key, value in self.userList:
+            if (checkvals(value) < self.compCutoff):
+                matchList.append(key)
         
-
+    def checkVals(userVals):
+        
 
 
 
