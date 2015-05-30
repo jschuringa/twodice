5496b03fef25d4d7c475fff0f1c51726cefdf344
Joey Schuringa <jschuringa86@gmail.com>
2015-05-30 10:53:41 -0500

    started search algo
    finishing tonight

--- search/search.py
+++ search/search.py
@@ -1,19 +1,18 @@
-class search:
 
-    matchList = []
 
-    def __init__(self, userList, compVals, compCutoff):
-        self.userList = userList
-        self.compVals = compVals
-        self.compCutoff = compCutoff
-        returnMatch()
+
+def startSearch(userVals, userSkills, jobSkills, compVals, compCutoff):
+    cultureMatchList = getMatchOnCulture(userList, compVals, compCutoff)
+    fullMatchList = getMatchOnSkills(cultureMatchList, userList)
     
-    def returnMatch():
-        for key, value in self.userList:
-            if (checkvals(value) < self.compCutoff):
-                matchList.append(key)
-        
-    def checkVals(userVals):
+def getMatchOnCulture(userLIst, compVals, compCutoff):
+    matchList = []
+    for key, value in self.userList:
+        if (checkvals(value) < self.compCutoff):
+            matchList.append(key)
+    return matchList  
+
+def checkVals(userVals):
         
 
 
