9391ab41fe505406f639792d4fcc19477733135d
Joey Schuringa <jschuringa86@gmail.com>
2015-06-06 19:54:01 -0500

    finished search

--- search/search.py
+++ search/search.py
@@ -1,19 +1,38 @@
+import operator
 
+def startSearch(userCulture, userSkills, compCultureDict, compJobDict):
+    matchDict = cultureMatch(userCulture, compCultureDict)
+    matchDict = skillMatch(matchDict, userSkills, compJobDict)
+    return matchDict
 
+def cultureMatch(userCulture, compCultureDict):
+    cultureMatchDict = {}
+    cultureScore = 0
+    for key, value in compCultureDict:
+        cultureScore = 0
+        for i in range(0, len(userCulture)):
+            for j in range(0, len(value)):
+                if userCulture[i] == value[j]:
+                    cultureScore += abs(i - j)
+        cultureMatchDict[key] = cultureScore
 
-def startSearch(userVals, userSkills, jobSkills, compVals, compCutoff):
-    cultureMatchList = getMatchOnCulture(userList, compVals, compCutoff)
-    fullMatchList = getMatchOnSkills(cultureMatchList, userList)
+    sortedList = sorted(cultureMatchDict.items(), key=operator.itemgetter(1))
+    return dict(sortedList)
+
     
-def getMatchOnCulture(userLIst, compVals, compCutoff):
-    matchList = []
-    for key, value in self.userList:
-        if (checkvals(value) < self.compCutoff):
-            matchList.append(key)
-    return matchList  
+def skillMatch(matchDict, userSkills, compJobDict):
+    jobMatchList = []
+    for key, value in matchDict:
+        for comp, dict in compJobDict:
+            if key == comp:
+                for job, skills in dict:
+                    for i in userSkills:
+                        for j in skills:
+                            if i == j:
+                                jobMatchList.append(job)
+            break
+    return jobMatchList
 
-def checkVals(userVals):
-        
 
 
 
